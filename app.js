cdac533801f97a23e280241b0dea2fd4202c3cd9
ashish pal singh <ashujss11@gmail.com>
2019-12-24 19:43:30 +0530

    new

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res) => {
-	res.send('Hello world');
+app.get("/", (req, res) => {
+  res.send("Hello world new");
 });
-app.listen(port,() => console.log('App is started on port ${port}'));
+app.listen(port, () => console.log("App is started on port ${port}"));
